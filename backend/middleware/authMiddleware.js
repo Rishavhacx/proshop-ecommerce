af204a79a5c23deccc1a7e99b62da1cfe5d681d9
Rishav Rishav <Rishav.Rishav@thameswater.co.uk>
2025-05-14 11:35:38 +0530

    added user functions and adding jwt in cookie

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "./asyncHandler.js";
+import User from "../models/userModel.js";
+
+// Protect routes
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  //Read the JWT from Cookie
+  token = req.cookies.jwt;
+
+  if(token) {
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decode.userId).select('-password');
+      next();
+    } catch (error) {
+    res.status(401);
+    throw new Error('Not authorized, token failed');
+    }
+  } else {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+
+//Admin middleware
+const admin = (req, res, next) => {
+    if(req.user && req.res.isAdmin){
+      next();  
+    } else {
+        res.status(401);
+        throw new Error('Not authorized as Admin');
+    }
+};
+
+export { protect, admin };
